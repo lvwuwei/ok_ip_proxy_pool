c97fd8b94df9c7f3523f8d944b64262c819aa687
guohy <lvwuwei@hotmail.com>
2021-03-23 21:32:13 +0100

    Update test_validator.py

--- test/validator/test_validator.py
+++ test/validator/test_validator.py
@@ -11,13 +11,13 @@ class TestValidator(unittest.TestCase):
     def setUp(self) -> None:
         self._opt = sqlite_opt
         self._validator = validator
-        # self._opt.init_db()
-        # proxy = ProxyEntity('127.0.0.1', '8080', source='66ip网', supplier='中国电信', proxy_type=ProxyTypeEnum.HTTPS.value)
-        # assert self._opt.add_proxy(proxy) == 1, '插入proxy表失败'
-        # proxy = ProxyEntity('127.0.0.2', '8081', source='66ip网', supplier='中国电信', proxy_type=ProxyTypeEnum.HTTPS.value)
-        # assert self._opt.add_proxy(proxy) == 1, '插入proxy表失败'
+        self._opt.init_db()
+        proxy = ProxyEntity('127.0.0.1', '8080', source='66ip网', supplier='中国电信', proxy_type=ProxyTypeEnum.HTTPS.value)
+        assert self._opt.add_proxy(proxy) == 1, '插入proxy表失败'
+        proxy = ProxyEntity('127.0.0.2', '8081', source='66ip网', supplier='中国电信', proxy_type=ProxyTypeEnum.HTTPS.value)
+        assert self._opt.add_proxy(proxy) == 1, '插入proxy表失败'
 
-        # self._opt.clean()
+        self._opt.clean()
 
     def test_valid_proxy(self):
         self._validator.run()
